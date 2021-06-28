63996e3746298116c3bd77fd3125e43438c24350
Athameon <Benjamin.Nopper@freenet.de>
2021-06-28 12:17:49 +0200

    Added get users from db functionality.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const db = require("../db/client");
 
 /* GET users listing. */
 router.get("/", (req, res, next) => {
-  res.send("respond with a resource");
+  db.query("SELECT * from users;").then(({ rows }) => {
+    res.send(rows);
+  });
+});
+router.get("/:id", (req, res, next) => {
+  db.query("SELECT * from users where id=$1;", [req.params.id]).then(
+    ({ rows }) => {
+      if (rows.length === 0) {
+        return res
+          .status(404)
+          .send("Could not find the user with the id: " + req.params.id);
+      }
+      res.send(rows[0]);
+    }
+  );
 });
 
 module.exports = router;
