0711910be26f24db922b18e3cfd633a97778bd51
Athameon <Benjamin.Nopper@freenet.de>
2021-06-28 13:23:20 +0200

    Added put functionality to update a user.

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,10 @@ const { body, validationResult } = require("express-validator");
 const router = express.Router();
 const db = require("../db/client");
 
+const getSingleUser = async (id) => {
+  return await db.query("SELECT * from users where id=$1;", [id]);
+};
+
 /* GET users listing. */
 router.get("/", (req, res, next) => {
   db.query("SELECT * from users;").then(({ rows }) => {
@@ -10,16 +14,14 @@ router.get("/", (req, res, next) => {
   });
 });
 router.get("/:id", (req, res, next) => {
-  db.query("SELECT * from users where id=$1;", [req.params.id]).then(
-    ({ rows }) => {
-      if (rows.length === 0) {
-        return res
-          .status(404)
-          .send("Could not find the user with the id: " + req.params.id);
-      }
-      res.send(rows[0]);
+  getSingleUser(req.params.id).then(({ rows }) => {
+    if (rows.length === 0) {
+      return res
+        .status(404)
+        .send("Could not find the user with the id: " + req.params.id);
     }
-  );
+    res.send(rows[0]);
+  });
 });
 
 const validateBody = [
@@ -50,6 +52,31 @@ router.post("/", validateBody, (req, res, next) => {
       console.error(error);
       res.status(500).send("Failed to create the new user.");
     });
+});
+
+router.put("/:id", validateBody, (req, res, next) => {
+  console.log("Request delete for user with id: ", req.params.id);
+  getSingleUser(req.params.id).then((result) => {
+    if (result.rows.length === 0) {
+      return res
+        .status(400)
+        .send(
+          `The user with the id ${req.params.id} does not exist in the db.`
+        );
+    }
+    const { firstname, lastname } = req.body;
+    db.query("UPDATE users SET firstname=$1, lastname=$2 RETURNING *", [
+      firstname,
+      lastname,
+    ])
+      .then((result) => {
+        res.send(result.rows[0]);
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send("Failed to update the user.");
+      });
+  });
 });
 
 module.exports = router;
